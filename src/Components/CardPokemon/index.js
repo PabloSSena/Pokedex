a38885a07d278c49f7d9ba6bdec856d2a6799ae3
PabloSSena <pablosilvasena@gmail.com>
2020-08-26 11:21:40 -0300

    Inicio do CardPokemon

---
+++ src/Components/CardPokemon/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function CardPokemon(){
+    return(
+        <div className="col-md-3 col-sm-6 nb-5">
+            <card>
+                <header>
+                    <h1>Teste</h1>
+                </header>
+            </card>
+        
+        </div>  
+    );
+
+}
+
+export default CardPokemon;
--- src/Components/Pokemons/index.js
+++ src/Components/Pokemons/index.js
@@ -3,11 +3,7 @@ import React from 'react';
 function Pokemons(){
     return(
         <> 
-        <ul>
-            <li>
-              
-            </li>
-          </ul>
+        
         </>  
     );
 
