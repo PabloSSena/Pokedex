251a602b2e34fa73266333931c3c85529f324bce
PabloSSena <pablosilvasena@gmail.com>
2020-08-26 16:54:23 -0300

    Feito o cardPokemon responsivo

--- src/Components/CardPokemon/index.js
+++ src/Components/CardPokemon/index.js
@@ -8,7 +8,6 @@ function CardPokemon(){
     return(
     <>    
         <container>
-
             <Card style={{ width: '15rem'}}>
              <Card.Img variant="top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png" />
              <Card.Body>
@@ -20,31 +19,27 @@ function CardPokemon(){
                 </Card.Body>
             </Card>
 
-            <card2>
-                <Card style={{ width: '15rem'}}>
-                <Card.Img variant="top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png" />
-                <Card.Body>
-                    <Card.Title>Bulbasauro</Card.Title>
-                        <Card.Text>
-                            Pokemon inicial
-                        </Card.Text>
-                        <Button variant="primary">Mais info</Button>
-                    </Card.Body>
-                </Card>
-            </card2>    
+            <Card style={{ width: '15rem'}}>
+             <Card.Img variant="top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png" />
+             <Card.Body>
+                 <Card.Title>Bulbasauro</Card.Title>
+                    <Card.Text>
+                        Pokemon inicial
+                    </Card.Text>
+                    <Button variant="primary">Mais info</Button>
+                </Card.Body>
+            </Card>
 
-            <card3>
-                <Card style={{ width: '15rem'}}>
-                <Card.Img variant="top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png" />
-                <Card.Body>
-                    <Card.Title>Bulbasauro</Card.Title>
-                        <Card.Text>
-                            Pokemon inicial
-                        </Card.Text>
-                        <Button variant="primary">Mais info</Button>
-                    </Card.Body>
-                </Card>
-            </card3>        
+            <Card style={{ width: '15rem'}}>
+             <Card.Img variant="top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png" />
+             <Card.Body>
+                 <Card.Title>Bulbasauro</Card.Title>
+                    <Card.Text>
+                        Pokemon inicial
+                    </Card.Text>
+                    <Button variant="primary">Mais info</Button>
+                </Card.Body>
+            </Card>
 
         </container>
     </>      
