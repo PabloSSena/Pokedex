5231d26fae2bf543bf6393d2557dbc4cd670925f
PabloSSena <pablosilvasena@gmail.com>
2020-08-26 14:30:28 -0300

    Configuraçao do grid feita testando 3 cards na tela

--- src/Components/CardPokemon/index.js
+++ src/Components/CardPokemon/index.js
@@ -8,8 +8,8 @@ function CardPokemon(){
     return(
     <>    
         <container>
-            <Card style={{ width: '15rem'}}>
 
+            <Card style={{ width: '15rem'}}>
              <Card.Img variant="top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png" />
              <Card.Body>
                  <Card.Title>Bulbasauro</Card.Title>
@@ -18,9 +18,34 @@ function CardPokemon(){
                     </Card.Text>
                     <Button variant="primary">Mais info</Button>
                 </Card.Body>
-            </Card>    
+            </Card>
 
-        
+            <card2>
+                <Card style={{ width: '15rem'}}>
+                <Card.Img variant="top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png" />
+                <Card.Body>
+                    <Card.Title>Bulbasauro</Card.Title>
+                        <Card.Text>
+                            Pokemon inicial
+                        </Card.Text>
+                        <Button variant="primary">Mais info</Button>
+                    </Card.Body>
+                </Card>
+            </card2>    
+
+            <card3>
+                <Card style={{ width: '15rem'}}>
+                <Card.Img variant="top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png" />
+                <Card.Body>
+                    <Card.Title>Bulbasauro</Card.Title>
+                        <Card.Text>
+                            Pokemon inicial
+                        </Card.Text>
+                        <Button variant="primary">Mais info</Button>
+                    </Card.Body>
+                </Card>
+            </card3>        
+
         </container>
     </>      
     );
