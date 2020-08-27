39e5ce0fedaa2e82fa64367c21cfa464ddaaf2c5
PabloSSena <pablosilvasena@gmail.com>
2020-08-27 13:43:05 -0300

    Adicionado cores para cada tipo de pokemon

--- src/Components/PokemonInfo/index.js
+++ src/Components/PokemonInfo/index.js
@@ -6,6 +6,27 @@ import Axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
+const CoresPraCadaTipo = {
+    bug: 'B1C12E',
+    dark: '4F3A2D',
+    dragon: '755EDF',
+    electric: 'FCBC17',
+    fairy: 'F4B1F4',
+    fighting: '823551D',
+    fire: 'E73B0C',
+    flying: 'A3B3F7',
+    ghost: '6060B2',
+    grass: '74C236',
+    ground: 'D3B357',
+    ice: 'A3E7FD',
+    normal: 'C8C4BC',
+    poison: '934594',
+    psychic: 'ED4882',
+    rock: 'B9A156',
+    steel: 'B5B5C3',
+    water: '3295F6'
+  };
+
 export default class PokemonInfo extends React.Component{
     state = {
         name:'',
@@ -103,18 +124,29 @@ export default class PokemonInfo extends React.Component{
            <div className='change-local'>
                 <Header/>
             </div>
-            <div className='container'>
+            {this.state.types.map(type =>(
+                <div className='container' style={{backgroundColor:`#${CoresPraCadaTipo[type]}`}}>
                 <div className='info'>
+
                     <Card>                
                         <Card.Img                 
                             variant="top"
                             src={this.state.imageurl}
                             />
                         </Card>
+                    <h4>#{this.state.pokemonIndex}</h4>
                     <h1>{this.state.name}</h1>    
-                
+
+                    <div className="tipos">
+                        {this.state.types.map(type => (
+                            <span key={type}
+                                className="badge  badge-pill mr1">{type}</span>
+                        ))}
+                    </div>
                 </div>    
             </div>
+            ))}
+            
             
          
            </> 
