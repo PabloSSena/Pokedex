02ddcf65e3dc5e7b581f2e2508180018017e9e05
PabloSSena <pablosilvasena@gmail.com>
2020-08-27 05:39:05 -0300

    Começando a puxar informaçoes da api para o PokemonInfo

--- src/Components/PokemonInfo/index.js
+++ src/Components/PokemonInfo/index.js
@@ -2,8 +2,37 @@ import React from 'react';
 import '../../styles/global.css';
 import './styles.css';
 import Header from '../Header';
+import Axios from 'axios';
 
 export default class PokemonInfo extends React.Component{
+    state = {
+        name:'',
+        pokemonIndex:'',
+        imagemurl:'',
+        types: [],
+        description:'',
+        stats:{
+            hp:'',
+            ataque:'',
+            defesa:'',
+            velocidade:'',
+            ataqEspecial:'',
+            defEspecial:''
+        }
+    }
+    async componentDidMount(){
+        const {pokemonIndex} = this.props.match.params;
+
+        const infoUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
+        const especiePokemon = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
+
+        const infoResponse = await Axios.get(infoUrl);
+
+        const name = infoResponse.data.name;
+        const imageurl = `https://pokeres.bastionbot.org/images/pokemon/${pokemonIndex}.png` 
+
+        let {hp,ataque,defesa,velocidade,ataqEspecial,defEspecial} = '';
+    }
     render(){
         return(
             <Header/>
