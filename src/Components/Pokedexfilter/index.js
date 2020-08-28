3fa5e5d04a6c2a74694ed0ca2f0f661cebd7b6e7
PabloSSena <pablosilvasena@gmail.com>
2020-08-27 23:48:56 -0300

    Testando algumas coisas pra tentar fazer chamada pelo nome

--- src/Components/Pokedexfilter/index.js
+++ src/Components/Pokedexfilter/index.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import Axios from 'axios';
 import './styles.css';
 
-function Pokedexfilter(){
+
+const App = () =>{
+    const pegarPokemon = async () =>{
+        const vetor=[];
+
+        try{
+          const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/pikachu';
+          const res = await Axios.get(pokemonUrl)
+          vetor.push()
+          console.log(res)
+        } catch(e){
+          console.log(e)
+        }
+    };
+
+    const handleChange = (e) => {
+      const pokemon = e.target.value.toLowerCase()
+      console.log(pokemon)
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      getPokemon();
+    }
+
     return(
         <pokedexfilter>
           <h1>Qual pokemon deseja<br/> encontrar?</h1>
           <inputcontrol>
-            <InputGroup className="mb-3">
+            <InputGroup className="mb-3" onSubmit={handleSubmit}>
             <FormControl
             placeholder="Nome do pokemon"
             aria-label="Nome do pokemon"
             aria-describedby="basic-addon2"
             />
-            <InputGroup.Append>
+            <InputGroup.Append onChange={handleChange}>
             <Button variant="outline-secondary">Pesquisar</Button>
             </InputGroup.Append>
             </InputGroup>
@@ -24,4 +49,4 @@ function Pokedexfilter(){
     );
 }
 
-export default Pokedexfilter;
+export default App;
